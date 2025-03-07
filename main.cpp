1cb748400573884d29f7858c764bab424ff0cb8a
azmine-1 <76449596+azmine-1@users.noreply.github.com>
2025-03-06 23:37:50 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,16 +2,7 @@
 #include <string>
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-
- 
-  std::cout << "$ ";
-
-  std::string input;
-  std::getline(std::cin, input);
-  if (!input.empty()) {
-    std::cout << input << ": command not found" << std::endl;
-  }
+    Shell shell;
+    shell.run();
+    return 0;
 }
