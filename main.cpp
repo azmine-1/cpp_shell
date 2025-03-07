2df0a8f2fed83c1e8a7fe722db9fc9c2aae244a3
azmine-1 <76449596+azmine-1@users.noreply.github.com>
2025-03-06 23:40:32 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -54,6 +54,35 @@ struct Command {
         bool append_output;
         std::vector<Command> pipe_commands;
     };
+std::string expand_env_vars(const std::string& token) {
+        std::string result = token;
+        size_t pos = 0;
+        
+        while ((pos = result.find('$', pos)) != std::string::npos) {
+            if (pos + 1 < result.length()) {
+                size_t end = pos + 1;
+                while (end < result.length() && (isalnum(result[end]) || result[end] == '_')) {
+                    end++;
+                }
+                
+                std::string var_name = result.substr(pos + 1, end - pos - 1);
+                std::string var_value;
+                
+                if (env_vars.find(var_name) != env_vars.end()) {
+                    var_value = env_vars[var_name];
+                } else if (const char* env = getenv(var_name.c_str())) {
+                    var_value = env;
+                }
+                
+                result.replace(pos, end - pos, var_value);
+                pos += var_value.length();
+            } else {
+                pos++;
+            }
+        }
+        
+        return result;
+    }
 
 int main() {
     Shell shell;
